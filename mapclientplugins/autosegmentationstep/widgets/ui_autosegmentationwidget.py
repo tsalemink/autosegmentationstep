e3e37b4e7a0fcd576b629fdf1f53f485c98f3ecb
TimmyNic <tim.nicolas@outlook.com>
2023-04-03 21:07:38 +1200

    Add slider labels

--- mapclientplugins/autosegmentationstep/widgets/ui_autosegmentationwidget.py
+++ mapclientplugins/autosegmentationstep/widgets/ui_autosegmentationwidget.py
@@ -16,8 +16,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QCheckBox, QGridLayout, QGroupBox,
-    QHBoxLayout, QPushButton, QSizePolicy, QSlider,
-    QSpacerItem, QVBoxLayout, QWidget)
+    QHBoxLayout, QLabel, QLayout, QPushButton,
+    QSizePolicy, QSlider, QSpacerItem, QVBoxLayout,
+    QWidget)
 
 from mapclientplugins.autosegmentationstep.widgets.zinc_scene import ZincScene
 
@@ -51,29 +52,70 @@ class Ui_AutoSegmentationWidget(object):
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
         self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        self.horizontalSpacer_2 = QSpacerItem(20, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
         self.horizontalLayout_2.addItem(self.horizontalSpacer_2)
+
+        self.verticalLayout_5 = QVBoxLayout()
+        self.verticalLayout_5.setSpacing(6)
+        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
+        self.verticalLayout_5.setSizeConstraint(QLayout.SetDefaultConstraint)
+        self.verticalLayout_5.setContentsMargins(5, -1, 5, -1)
+        self.label = QLabel(self.groupBox)
+        self.label.setObjectName(u"label")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy2)
+        self.label.setMinimumSize(QSize(80, 26))
+        self.label.setMaximumSize(QSize(80, 26))
+        self.label.setAlignment(Qt.AlignCenter)
+        self.label.setWordWrap(True)
+
+        self.verticalLayout_5.addWidget(self.label)
 
         self.isoValueSlider = QSlider(self.groupBox)
         self.isoValueSlider.setObjectName(u"isoValueSlider")
         self.isoValueSlider.setMaximum(99)
         self.isoValueSlider.setOrientation(Qt.Vertical)
 
-        self.horizontalLayout_2.addWidget(self.isoValueSlider)
+        self.verticalLayout_5.addWidget(self.isoValueSlider, 0, Qt.AlignHCenter)
 
-        self.horizontalSpacer_3 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_2.addLayout(self.verticalLayout_5)
+
+        self.horizontalSpacer_3 = QSpacerItem(20, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
         self.horizontalLayout_2.addItem(self.horizontalSpacer_3)
+
+        self.verticalLayout_4 = QVBoxLayout()
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.label_2 = QLabel(self.groupBox)
+        self.label_2.setObjectName(u"label_2")
+        sizePolicy3 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        sizePolicy3.setHorizontalStretch(0)
+        sizePolicy3.setVerticalStretch(0)
+        sizePolicy3.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
+        self.label_2.setSizePolicy(sizePolicy3)
+        self.label_2.setMinimumSize(QSize(90, 26))
+        self.label_2.setMaximumSize(QSize(90, 26))
+        self.label_2.setAlignment(Qt.AlignCenter)
+        self.label_2.setWordWrap(True)
+
+        self.verticalLayout_4.addWidget(self.label_2)
 
         self.segmentationValueSlider = QSlider(self.groupBox)
         self.segmentationValueSlider.setObjectName(u"segmentationValueSlider")
         self.segmentationValueSlider.setMaximum(10000)
         self.segmentationValueSlider.setOrientation(Qt.Vertical)
 
-        self.horizontalLayout_2.addWidget(self.segmentationValueSlider)
+        self.verticalLayout_4.addWidget(self.segmentationValueSlider, 0, Qt.AlignHCenter)
 
-        self.horizontalSpacer_5 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_2.addLayout(self.verticalLayout_4)
+
+        self.horizontalSpacer_5 = QSpacerItem(20, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
         self.horizontalLayout_2.addItem(self.horizontalSpacer_5)
 
@@ -132,6 +174,8 @@ class Ui_AutoSegmentationWidget(object):
     def retranslateUi(self, AutoSegmentationWidget):
         AutoSegmentationWidget.setWindowTitle(QCoreApplication.translate("AutoSegmentationWidget", u"Form", None))
         self.groupBox.setTitle(QCoreApplication.translate("AutoSegmentationWidget", u"Auto Segmentation Viewer", None))
+        self.label.setText(QCoreApplication.translate("AutoSegmentationWidget", u"Image Plane Level", None))
+        self.label_2.setText(QCoreApplication.translate("AutoSegmentationWidget", u"Segmentation Contour Threshold", None))
         self.imagePlaneCheckBox.setText(QCoreApplication.translate("AutoSegmentationWidget", u"Image Plane", None))
         self.segmentationCheckBox.setText(QCoreApplication.translate("AutoSegmentationWidget", u"Segmentation", None))
         self.pointCloudCheckBox.setText(QCoreApplication.translate("AutoSegmentationWidget", u"Point Cloud", None))
